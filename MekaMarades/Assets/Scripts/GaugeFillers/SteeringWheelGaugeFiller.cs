cc129bb065503cc93e1e95915e9ff5bf8c4c132b
p.luneau <paul.luneau@mail.com>
2024-09-27 01:26:41 +0200

    Did a bit of cleaning I think

--- MekaMarades/Assets/Scripts/GaugeFillers/SteeringWheelGaugeFiller.cs
+++ MekaMarades/Assets/Scripts/GaugeFillers/SteeringWheelGaugeFiller.cs
@@ -3,18 +3,33 @@ using UnityEngine;
 
 public class SteeringWheelGaugeFiller : GaugeFiller
 {
-    [SerializeField] private float m_fillingSpeed = 10f;
     [SerializeField] private SteeringWheelGame m_steeringWheelGame;
-
-    [NonSerialized] private float m_currentNeedlePosition;
-    [NonSerialized] private float m_currentScoringZonePosition;
-
+    [SerializeField] private float m_fillingSpeed = 10f;
+    [SerializeField] private float m_timeBeforeScoring = 0.5f;
 
     [NonSerialized] private float m_timeSpentInScoringZone;
+
+    private void Start()
+    {
+        m_timeSpentInScoringZone = 0.0f;
+    }
 
 
     public override float GetAdditionalFilling(float deltaTime)
     {
-        return m_steeringWheelGame.IsNeedleInScoringBounds() ? m_fillingSpeed*0.001f : 0f;
+        if (!m_steeringWheelGame.IsNeedleInScoringBounds())
+        {
+            m_timeSpentInScoringZone = 0.0f;
+            return 0.0f;
+        }
+
+        m_timeSpentInScoringZone += deltaTime;
+
+        if (m_timeSpentInScoringZone < m_timeBeforeScoring)
+        {
+            return 0.0f;
+        }
+
+        return m_fillingSpeed * 0.001f;
     }
 }
--- MekaMarades/Assets/Scripts/SteeringWheelGame.cs
+++ MekaMarades/Assets/Scripts/SteeringWheelGame.cs
@@ -1,19 +1,25 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityUtility.CustomAttributes;
+using UnityUtility.Utils;
+
+using Random = UnityEngine.Random;
 
 public class SteeringWheelGame : MonoBehaviour
 {
     [SerializeField] private InputActionReference m_wheelAction;
-    
-    [Header("Needle Parameters")]
-    [SerializeField, Range(0,1)] private float m_needleSpeed;
-    [SerializeField, Range(0,100)] private float m_needleWidth;
-    [Header("ScoringZone Parameters")]
-    [SerializeField, Range(0,1)] private float m_scoringZoneSpeed;
-    [SerializeField, Range(0,100)] private float m_scoringZoneWidth;
 
-    [Header("Transforms")]
+    [Title("Needle Parameters")]
+    [SerializeField, Range(0, 5)] private float m_needleSpeed;
+    [SerializeField, Range(0, 100)] private float m_needleWidth;
+
+    [Title("ScoringZone Parameters")]
+    [SerializeField, Range(0, 5)] private float m_scoringZoneSpeed;
+    [SerializeField, Range(0, 100)] private float m_scoringZoneWidth;
+    [SerializeField, Range(0, 1)] private float m_scoringZoneTargetTolerance;
+
+    [Title("Transforms")]
     [SerializeField] private RectTransform m_needle;
     [SerializeField] private RectTransform m_scoringZone;
     [SerializeField] private RectTransform m_backgroundZone;
@@ -22,6 +28,8 @@ public class SteeringWheelGame : MonoBehaviour
     [NonSerialized] private float m_needlePosition;
     [NonSerialized] private float m_scoringZonePosition;
     [NonSerialized] private float m_backgroundZoneWidth;
+
+    [NonSerialized] private float m_scoringZoneTarget;
 
     //[NonSerialized] private Vector2 m_scoringZoneRange;
 
@@ -32,13 +40,14 @@ public class SteeringWheelGame : MonoBehaviour
     {
         m_needlePosition = 0.5f;
         m_scoringZonePosition = 0.5f;
-        
+
 
         m_backgroundZoneWidth = m_backgroundZone.rect.width;
-        m_needle.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth*m_needleWidth/100);
-        m_scoringZone.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth*m_scoringZoneWidth/100);
+        m_needle.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth * m_needleWidth / 100);
+        m_scoringZone.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth * m_scoringZoneWidth / 100);
 
-        
+        m_scoringZoneTarget = GetNewScoringZoneTarget();
+
         MoveScoringZone();
 
         //m_scoringZoneRange = new Vector2(m_scoringZoneWidth / 2, 1 - (m_scoringZoneWidth / 2));
@@ -53,26 +62,40 @@ public class SteeringWheelGame : MonoBehaviour
 
     private void MoveScoringZone()
     {
-        m_scoringZonePosition += UnityEngine.Random.Range(-1f, 1f)*Time.deltaTime*m_scoringZoneSpeed;
-        m_scoringZonePosition = Mathf.Clamp01(m_scoringZonePosition);
+        if (m_scoringZonePosition.Approximately(m_scoringZoneTarget, m_scoringZoneTargetTolerance))
+        {
+            m_scoringZoneTarget = GetNewScoringZoneTarget();
+        }
+
+        float distToTarget = m_scoringZoneTarget - m_scoringZonePosition;
+        m_scoringZonePosition += Mathf.Min(Mathf.Abs(distToTarget), m_scoringZoneSpeed * Time.deltaTime) * Mathf.Sign(distToTarget);
+
+
+        //m_scoringZonePosition += Random.Range(-1f, 1f) * Time.deltaTime * m_scoringZoneSpeed;
+        //m_scoringZonePosition = Mathf.Clamp01(m_scoringZonePosition);
         m_scoringZone.anchoredPosition = new Vector3(GetInGamePosition(m_scoringZone, m_scoringZonePosition), 0, 0);
+    }
+
+    private float GetNewScoringZoneTarget()
+    {
+        return Random.value;
     }
 
     private void MoveCursor(float speed)
     {
-        m_needlePosition += m_needleSpeed*speed*0.01f;
+        m_needlePosition += m_needleSpeed * speed * 0.01f;
         m_needlePosition = Mathf.Clamp01(m_needlePosition);
         m_needle.anchoredPosition = new Vector3(GetInGamePosition(m_needle, m_needlePosition), 0, 0);
     }
 
     private float GetInGamePosition(RectTransform rectTransform, float virtualPosition)
     {
-        float rectHalfWidth = rectTransform.rect.width/2;
-        return Mathf.Lerp(rectHalfWidth, m_backgroundZoneWidth-rectHalfWidth, virtualPosition);
+        float rectHalfWidth = rectTransform.rect.width / 2;
+        return Mathf.Lerp(rectHalfWidth, m_backgroundZoneWidth - rectHalfWidth, virtualPosition);
     }
 
     public bool IsNeedleInScoringBounds()
     {
-        return Mathf.Abs(m_needlePosition - m_scoringZonePosition) < Mathf.Abs(m_scoringZoneWidth - m_needleWidth)/200;
+        return Mathf.Abs(m_needlePosition - m_scoringZonePosition) < Mathf.Abs(m_scoringZoneWidth - m_needleWidth) / 200;
     }
 }
