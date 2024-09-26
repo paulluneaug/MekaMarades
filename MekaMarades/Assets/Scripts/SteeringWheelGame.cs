fd2dfd65e1833c8cc49e394b1079717b0e368d66
Corentin Boblet <corentinboblet@gmail.com>
2024-09-27 01:25:01 +0200

    ScoringZone movement is funnier to play

--- MekaMarades/Assets/Scripts/SteeringWheelGame.cs
+++ MekaMarades/Assets/Scripts/SteeringWheelGame.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Timers;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = Unity.Mathematics.Random;
 
 public class SteeringWheelGame : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class SteeringWheelGame : MonoBehaviour
     [Header("ScoringZone Parameters")]
     [SerializeField, Range(0,1)] private float m_scoringZoneSpeed;
     [SerializeField, Range(0,100)] private float m_scoringZoneWidth;
+    [SerializeField, Range(0,5)] private float m_minNextScoringZoneTime;
+    [SerializeField, Range(0,5)] private float m_maxNextScoringZoneTime;
 
     [Header("Transforms")]
     [SerializeField] private RectTransform m_needle;
@@ -22,6 +26,12 @@ public class SteeringWheelGame : MonoBehaviour
     [NonSerialized] private float m_needlePosition;
     [NonSerialized] private float m_scoringZonePosition;
     [NonSerialized] private float m_backgroundZoneWidth;
+    
+    [NonSerialized] private float m_scoringZoneNextPosition;
+    [NonSerialized] private Timer m_scoringZoneTimer = new();
+    
+    
+    [NonSerialized] private Random m_random;
 
     //[NonSerialized] private Vector2 m_scoringZoneRange;
 
@@ -30,18 +40,29 @@ public class SteeringWheelGame : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        // Random
+        m_random.InitState();
+        
+        // Default positions
         m_needlePosition = 0.5f;
         m_scoringZonePosition = 0.5f;
-        
 
+        // Scoring Timer
+        m_scoringZoneTimer.AutoReset = false;
+        m_scoringZoneTimer.Elapsed += FindNextScoringPosition;
+        m_scoringZoneTimer.Start();
+        
+        // Set Timer limits
+        m_minNextScoringZoneTime *= 1000;
+        m_maxNextScoringZoneTime *= 1000;
+
+        // Set width
         m_backgroundZoneWidth = m_backgroundZone.rect.width;
         m_needle.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth*m_needleWidth/100);
         m_scoringZone.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_backgroundZoneWidth*m_scoringZoneWidth/100);
 
         
         MoveScoringZone();
-
-        //m_scoringZoneRange = new Vector2(m_scoringZoneWidth / 2, 1 - (m_scoringZoneWidth / 2));
     }
 
     // Update is called once per frame
@@ -53,9 +74,23 @@ public class SteeringWheelGame : MonoBehaviour
 
     private void MoveScoringZone()
     {
-        m_scoringZonePosition += UnityEngine.Random.Range(-1f, 1f)*Time.deltaTime*m_scoringZoneSpeed;
-        m_scoringZonePosition = Mathf.Clamp01(m_scoringZonePosition);
+        if (Mathf.Abs(m_scoringZonePosition - m_scoringZoneNextPosition) < 0.01f)
+        {
+            return;
+        }
+
+        m_scoringZonePosition = Mathf.Lerp(m_scoringZonePosition, m_scoringZoneNextPosition, m_scoringZoneSpeed/10);
         m_scoringZone.anchoredPosition = new Vector3(GetInGamePosition(m_scoringZone, m_scoringZonePosition), 0, 0);
+    }
+
+    private void FindNextScoringPosition(object _sender, ElapsedEventArgs _elapsedEventArgs)
+    {
+        Debug.Log(m_scoringZoneNextPosition);
+        m_scoringZoneTimer.Stop();
+        m_scoringZoneNextPosition = m_random.NextFloat(0f, 1f);
+        m_scoringZoneTimer.Interval = m_random.NextFloat(m_minNextScoringZoneTime, m_maxNextScoringZoneTime);
+        m_scoringZoneTimer.Start();
+        Debug.Log(m_scoringZoneNextPosition);
     }
 
     private void MoveCursor(float speed)
